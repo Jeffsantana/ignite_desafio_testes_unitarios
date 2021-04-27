8561b3aa3bf80111ecdc8d931e218f461d86f6b4
Jeffsantana <jeffersonsantana2008@hotmail.com>
2021-04-27 15:31:19 -0300

    tests of user

---
+++ src/__tests__/users/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,26 @@
+import { InMemoryUsersRepository } from "../../modules/users/repositories/in-memory/InMemoryUsersRepository"
+import { AuthenticateUserUseCase } from "../../modules/users/useCases/authenticateUser/AuthenticateUserUseCase";
+import { CreateUserUseCase } from "../../modules/users/useCases/createUser/CreateUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let authenticateUserUserCase: AuthenticateUserUseCase;
+let createUser: CreateUserUseCase;
+
+describe("Verify authenticate", () => {
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository();
+        authenticateUserUserCase = new AuthenticateUserUseCase(inMemoryUsersRepository);
+        createUser = new CreateUserUseCase(inMemoryUsersRepository);
+    })
+    it("Should be able to create a session", async () => {
+        const newUser = {
+            name: "User Test",
+            email: "user@newuser.com",
+            password: "112358"
+        }
+        await createUser.execute(newUser);
+        const authenticate = await authenticateUserUserCase.execute(newUser)
+        console.log("🚀 ~ authenticate", authenticate);
+        expect(authenticate).toHaveProperty("token")
+    })
+})
